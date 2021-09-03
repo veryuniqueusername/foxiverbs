1f7674492784206a859e775df25f750b7dceb5e3
veryuniqueusername <70266105+veryuniqueusername@users.noreply.github.com>
2021-09-03 09:03:45 +0200

    fix order

--- src/Choose.tsx
+++ src/Choose.tsx
@@ -9,13 +9,10 @@ for (let i = 0; i < verbList.length; i++) {
 	let sp = '';
 	let pp = '';
 	sp += verbConjugations[verbList[i]['eng']][0][0];
-	pp += verbConjugations[verbList[i]['eng']][0][0];
+	pp += verbConjugations[verbList[i]['eng']][0][1];
 	if (verbConjugations[verbList[i]['eng']][1][0]) {
 		sp += ` / ${verbConjugations[verbList[i]['eng']][1][0]}`;
-	}
-
-	if (verbConjugations[verbList[i]['eng']][0][1]) {
-		pp += ` / ${verbConjugations[verbList[i]['eng']][0][1]}`;
+		pp += ` / ${verbConjugations[verbList[i]['eng']][1][1]}`;
 	}
 
 	json[`${verbList[i]['eng']}`] = [sp, pp, verbList[i]['swe']];
